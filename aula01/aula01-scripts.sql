30c4360c636fe98d4c66cc17eeeddb7df4ff3961
lucascagostinho <lucascagostinho@gmail.com>
2022-06-22 21:22:08 -0300

    scripts aula01

---
+++ aula01/aula01-scripts.sql
@@ -0,0 +1,24 @@
+CREATE TABLE pessoa (
+    id    INTEGER PRIMARY KEY AUTOINCREMENT
+                  UNIQUE,
+    nome  TEXT    NOT NULL,
+    idade INTEGER
+);
+
+-- ddl para criar uma tabela
+-- ddl manipula objetos do banco de dados como tabelas e colunas
+CREATE TABLE usuario(
+id_usuario INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
+matricula TEXT NOT NULL UNIQUE,
+nome TEXT NOT NULL
+);
+
+-- dml manipulação do banco de dados como inserts, update, delete, select
+-- verificando se a tabela existe
+SELECT * FROM pessoa;
+
+-- inserindo um registro na tabela pessoa
+INSERT INTO pessoa(NOME, IDADE) VALUES("Lucas", 25); -- para inserir é necessário informar os campos e os valores que os campos recebem
+
+-- delete
+DELETE FROM pessoa;
